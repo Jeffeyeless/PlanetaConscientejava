32b064760d5f1614e7cfa43cfdd3b2f1e7c27979
jeffeyeless <jeffedeel@gmail.com>
2025-12-09 00:20:47 -0500

    a

--- src/main/java/com/app/planetaconsciente/config/SecurityConfig.java
+++ src/main/java/com/app/planetaconsciente/config/SecurityConfig.java
@@ -20,9 +20,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            )
+            .csrf(AbstractHttpConfigurer::disable)
+            
             .headers(headers -> headers
                 .frameOptions().sameOrigin()
             )
